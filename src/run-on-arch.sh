af76c2f60c62cf4b6c9785b40d35609f4984a07d
Rajas Kakodkar <Rajas.Kakodkar@ibm.com>
2020-05-06 16:55:25 +0530

    Use 4.2.0-6 qemu version

--- src/run-on-arch.sh
+++ src/run-on-arch.sh
@@ -7,10 +7,15 @@ DISTRO=$2
 COMMANDS=$3
 COMMANDS="${COMMANDS//[$'\t\r\n']+/;}" #Replace newline with ;
 ADDITIONAL_ARGS=$4
+QEMU_VERSION=4.2.0-6
 
 # Install support for new archs via qemu
 # Platforms: linux/amd64, linux/arm64, linux/riscv64, linux/ppc64le, linux/s390x, linux/386, linux/arm/v7, linux/arm/v6
-sudo apt update -y && sudo apt install -y qemu qemu-user-static
+#sudo apt update -y && sudo apt install -y qemu qemu-user-static
+
+sudo apt update -y 
+curl -L https://github.com/multiarch/qemu-user-static/releases/download/v${QEMU_VERSION}/qemu-$ARCH-static.tar.gz 
+sudo tar zxvf - -C /usr/bin
 
 ACT_PATH=$(dirname $(dirname $(readlink -fm "$0")))
 
