98eac7fd23b93a7103989cd72421f2223568b9db
Rajas Kakodkar <rajas.kakodkar@ibm.com>
2020-05-11 11:53:46 +0530

    Update run-on-arch.sh

--- src/run-on-arch.sh
+++ src/run-on-arch.sh
@@ -27,6 +27,7 @@ docker build . --file $ACT_PATH/Dockerfiles/Dockerfile.$ARCH.$DISTRO --tag multi
 sudo apt update -y && sudo apt install -y git
 
 git clone https://github.com/rajaskakodkar/cadvisor.git
+cd cadvisor
 git checkout s390x-integration-test
 
 GORACE="halt_on_error=1" ./cadvisor --docker_env_metadata_whitelist=TEST_VAR --v=6 --logtostderr $CADVISOR_ARGS &
