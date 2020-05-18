4a4c2ec044070aafbfe2732c07f57225e33e6480
Rajas Kakodkar <rajas.kakodkar@ibm.com>
2020-05-18 16:41:23 +0530

    Update run-on-arch.sh

--- src/run-on-arch.sh
+++ src/run-on-arch.sh
@@ -11,17 +11,11 @@ QEMU_VERSION=4.2.0-6
 
 # Install support for new archs via qemu
 # Platforms: linux/amd64, linux/arm64, linux/riscv64, linux/ppc64le, linux/s390x, linux/386, linux/arm/v7, linux/arm/v6
-#sudo apt update -y && sudo apt install -y qemu qemu-user-static
-
-#sudo apt update -y 
-#curl -L https://github.com/multiarch/qemu-user-static/releases/download/v${QEMU_VERSION}/x86_64_qemu-s390x-static.tar.gz | tar zxvf - -C /usr/bin
-#chmod +x /usr/bin/qemu-*
-#curl -L https://github.com/multiarch/qemu-user-static/releases/download/v${QEMU_VERSION}/qemu-$ARCH-static.tar.gz 
-#sudo tar zxvf qemu-$ARCH-static.tar.gz -C /usr/bin
+sudo apt update -y && sudo apt install -y qemu qemu-user-static
 
 ACT_PATH=$(dirname $(dirname $(readlink -fm "$0")))
 
-docker run --rm --privileged multiarch/qemu-user-static:4.2.0-6 --reset -p yes
+docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 docker build . --file $ACT_PATH/Dockerfiles/Dockerfile.$ARCH.$DISTRO --tag multiarchimage 
 
 sudo apt update -y && sudo apt install -y git
